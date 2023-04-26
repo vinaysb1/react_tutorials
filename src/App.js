535753e14f5333c6883bf40dbbf0e32c833ba097
vinay <vinaykumarbeelagi@gmail.com>
2023-04-26 20:01:29 +0530

    Refs with classcomponent

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Form from './Components/Form';
 import LifecycleA from './Components/LifecycleA';
 import ParentComp from './Components/ParentComp';
 import Refsdemo from './Components/Refsdemo';
+import FocusInput from './Components/FocusInput';
 
 function App() {
   return (
     <div className="App">
-		<Refsdemo />
+		<FocusInput />
+		{/* <Refsdemo /> */}
 		{/* <ParentComp /> */}
 			{/*  <Form /> */}
       {/* <LifecycleA /> */}
